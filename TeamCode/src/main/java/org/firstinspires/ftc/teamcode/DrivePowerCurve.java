c17afcc440d48ea253594b02cf32bf136a724a22
Nuri <chip.marklar@gmail.com>
2018-10-29 16:49:51 -0700

    Adding different power curves

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivePowerCurve.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class DrivePowerCurve {
+
+    public static double threePartPoly(double stickPositon){
+        return (Math.pow(stickPositon,0.33333333) + Math.pow(stickPositon,3) + Math.pow(stickPositon,5)) /3;
+    }
+
+    public static float valueSquared(float stickPosition) {
+        return Math.abs(stickPosition) * stickPosition;
+    }
+}
